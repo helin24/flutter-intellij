b828e8d04e742417be9a31efcc8749a8b05bd06d
Helin Shiah <helinx@google.com>
2020-08-25 09:04:56 -0700

    Use flutter-intellij-kokoro2 for testing

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -14,6 +14,6 @@ set -e
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
-cd ${KOKORO_ARTIFACTS_DIR}/github/flutter-intellij-kokoro
+cd ${KOKORO_ARTIFACTS_DIR}/github/flutter-intellij-kokoro2
 
 ./tool/kokoro/build.sh
